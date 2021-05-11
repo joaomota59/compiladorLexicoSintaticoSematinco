239d67050bc6b3104426c7eea600f83357f9b6e0
João Lucas Mota <joaomota59@hotmail.com>
2021-05-11 10:49:38 -0300

    add regras sintaticas

--- compilador/index.py
+++ compilador/index.py
@@ -1,20 +1,21 @@
 #pip install sly
-from sly import Lexer
+from sly import Lexer, Parser
+import pprint
 
-class CalcLexer(Lexer):
+
+class VisualgLexer(Lexer):    
 
     # Set of token names.   This is always required
     tokens = {
-    ALGORITMO,VAR,VERDADEIRO,FALSO,FUNCAO,FIMFUNCAO,ID,REAL,INTEIRO,
+    ALGORITMO,FIMALGORITMO,VAR,VERDADEIRO,FALSO,FUNCAO,FIMFUNCAO,ID,REAL,INTEIRO,
     CARACTERE,LOGICO, SE, ENTAO,SENAO,FIMSE,ENQUANTO,FACA,REPITA,FIMREPITA,
-    FIMENQUANTO,ESCREVA,E,OU,NAO,LEIA, LIMPATELA, PROCEDIMENTO,
+    FIMENQUANTO,ESCREVA,ESCREVAL,E,OU,NAO,LEIA, LIMPATELA, PROCEDIMENTO,
     FIMPROCEDIMENTO, ESCOLHA, FIMESCOLHA, VETOR, ASC, TIMER, CARAC, ECO,
-    LE, LT ,GE, GT,EQ, NE, ASSIGN,MOD, RETORNE, POS, XOU, CRONOMETRO
+    LE, LT ,GE, GT,EQ, NE, ASSIGN,MOD, RETORNE, POS, XOU, CRONOMETRO,INICIO
     }#Nome dos tokens devem estar em maiusculo
     #O valor de cada chave desse dicionario sera definido durante a execução
 
-
-    literals = {'+','-','*','/','^','(', ')', ':', '\\', '%'} #caracteres literais, o token é representado pelo proprio simbolo
+    literals = {'+','-','*','/','^','(', ')', ',',':', '\\', '%'} #caracteres literais, o token é representado pelo proprio simbolo
 
 
     ###Caracteres Ignorados###
@@ -27,7 +28,7 @@ class CalcLexer(Lexer):
     ####################################################
 
     # Regular expression rules for tokens -- Aqui será definido as expressoes regulares que caracterizam os tokens definidos antes
-    REAL = r'(real|[0-9]+.[0-9]+)'#se for a palavra reservada ou (um conjunto de digitos . conjunto de digitos) é do tipo real
+    REAL = r'(real|[0-9]+[.][0-9]*)'#se for a palavra reservada ou (um conjunto de digitos . conjunto de digitos) é do tipo real
     INTEIRO = r'(inteiro|[0-9]+)' #se for a palavra reservada ou um conjunto de digitos é do tipo inteiro
     CARACTERE = r'(caractere|"[^\n]+"|"")'#se for a palavra reservada ou qualquer coisa que n seja \n no texto entao é caracter
     ID      = r'[a-zA-Z_][a-zA-Z0-9_]*' #variavel que começa com alguma letra maiuscula ou minuscula
@@ -40,10 +41,12 @@ class CalcLexer(Lexer):
     EQ     = r'==' #comparação
     
 
-    # Caracteres especiais / palavras reservadas / Definir todas aqui
-    ID['var'] = VAR
+    # Caracteres especiais / palavras reservadas / Definir todas aqui na parte de remapeamento de token
+    ID['Var'] = VAR
+    ID['Inicio'] = INICIO
+    ID['Fimalgoritmo'] = FIMALGORITMO
     ID['MOD'] = MOD
-    ID['algoritmo'] = ALGORITMO
+    ID['Algoritmo'] = ALGORITMO
     ID['verdadeiro'] = VERDADEIRO
     ID['falso'] = FALSO
     ID['funcao'] = FUNCAO
@@ -63,6 +66,7 @@ class CalcLexer(Lexer):
     ID['senao'] = SENAO
     ID['fimse'] = FIMSE
     ID['escreva'] = ESCREVA
+    ID['escreval'] = ESCREVAL
     ID['logico'] = LOGICO
     ID['e'] = E 
     ID['ou'] = OU
@@ -86,11 +90,66 @@ class CalcLexer(Lexer):
         print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
         self.index += 1
 
+
+    @_(r'[0-9]+[.][0-9]*')
+    def REAL(self,t):
+        t.value = float(t.value)   # Converte para um valor real
+        return t
+
+    @_(r'[0-9]+')
+    def INTEIRO(self, t):
+        t.value = int(t.value)   # Converte para um valor inteiro
+        return t
+
+
+class VisualgParser(Parser):
+    tokens = VisualgLexer.tokens
+
+ #Regras gramaticais para numeros inteiros
+    @_('expr "+" term')
+    def expr(self, p):
+        #return
+        return p.expr + p.term
+
+    @_('expr "-" term')
+    def expr(self, p):
+        #return
+        return p.expr - p.term
+
+    @_('term')
+    def expr(self, p):
+        #return
+        return p.term
+
+    @_('factor')
+    def term(self, p):
+        #return
+        return p.factor
+
+    @_('REAL')
+    def factor(self, p):
+        #return
+        return p.REAL
+    @_('INTEIRO')
+    def factor(self, p):
+        #return
+        return p.INTEIRO
+    @_('"(" expr ")"')
+    def factor(self, p):
+        return p.expr
+###OBS FALTA DIVISAO ENTRE REAL!!
+
+
+
+
 if __name__ == '__main__':
     arquivo = open('algoritmo.txt', 'r')
     data = arquivo.read()
     arquivo.close()
-    lexer = CalcLexer()
-    for tok in lexer.tokenize(data):
-        print('type=%r, value=%r' % (tok.type, tok.value))
+    lexer = VisualgLexer()
+    parser = VisualgParser()
+    resultado = parser.parse(lexer.tokenize(data))
+    pprint.pprint(resultado)
+    #for tok in lexer.tokenize(data):
+    #    print('type=%r, value=%r' % (tok.type, tok.value))
     
